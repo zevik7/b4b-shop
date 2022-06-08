3d7320e77c166c3c45ab89fa8065f30b2738436f
Zevik <58850643+zevik7@users.noreply.github.com>
2022-06-08 15:25:41 +0700

    feat: setup env, less-loader with craco and redux toolkit
    * feat: setup less-loader with craco
    
    * feat: setup redux toolkit
    
    * feat: setup router

---
+++ src/pages/Home/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Home = () => {
+  return <h1>This is Homepage</h1>
+}
+
+export default Home
---
+++ src/redux/reducer.js
@@ -0,0 +1,13 @@
+// Example
+
+// import { combineReducers } from "redux";
+
+// import filtersReducer from "../components/Filters/FilterSlide";
+// import todoListReducer from "../components/TodoList/TodosSlide";
+
+// const rootReducer = combineReducers({
+// 	filters: filtersReducer,
+// 	todoList: todoListReducer,
+// });
+
+// export default rootReducer;
