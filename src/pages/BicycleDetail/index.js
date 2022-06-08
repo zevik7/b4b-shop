edb84029a44ca19380ace38aac0db8533ecb65b5
Zevik <58850643+zevik7@users.noreply.github.com>
2022-06-08 16:10:03 +0700

    chore: change primary color

--- src/pages/BicycleDetail/index.js
+++ src/pages/BicycleDetail/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import './style.less'
 
 const BicycleManagement = () => {
-  return <h1>This is Bicycle Detail</h1>
+  return <h1 className="bicycle-detail">This is Bicycle Detail</h1>
 }
 
 export default BicycleManagement
