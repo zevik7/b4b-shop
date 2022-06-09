5f2467391c124dd0a9976b82e872ffd2d7d2852d
truc ho <truc.ho@nfq.asia>
2022-06-09 15:48:07 +0700

    feat: add bicycle list for homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './index.less';
+import { Row, Col } from 'antd';
+import BicycleGrid from '../../components/BicycleGrid';
+import BicycleSearch from '../../components/BicycleSearch';
+import BicyclePagination from '../../components/BicyclePagination';
 
 const Home = () => {
-  return <h1>This is Homepage</h1>
+  return (
+    <div className='homepage'>
+    <h1>Catalog</h1>
+    <Col>
+      <Row className='product-search'><BicycleSearch/></Row>
+      <Row className='product-grid'><BicycleGrid/></Row>
+      <Row className='product-pagination'><BicyclePagination/></Row>
+    </Col>
+    </div>
+  )
 }
 
 export default Home
