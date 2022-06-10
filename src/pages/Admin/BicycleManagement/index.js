3383644a3d21eb2f33f41d783286f8078969b89d
Huynh Van Gioi Em <104955752+gioiemhuynh@users.noreply.github.com>
2022-06-10 11:44:16 +0700

    feat: design the layout for bicycle management page

--- src/pages/Admin/BicycleManagement/index.js
+++ src/pages/Admin/BicycleManagement/index.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons'
+import { Layout, Menu } from 'antd'
+import React, { useState } from 'react'
+import './style.less'
+import { BicycleTable, HeaderComponent, Sidebar } from '../../../components'
+
+const { Header, Sider, Content } = Layout
 
 const BicycleManagement = () => {
-  return <h1>This is Bicycle</h1>
+  const [collapsed, setCollapsed] = useState(false)
+  return (
+    <Layout>
+      <Sidebar collapsed={collapsed} />
+      <Layout className="site-layout">
+        <HeaderComponent collapsed={collapsed} toggleSidebar={setCollapsed} />
+        <Content className="site-layout-background">
+          <BicycleTable />
+        </Content>
+      </Layout>
+    </Layout>
+  )
 }
 
 export default BicycleManagement
