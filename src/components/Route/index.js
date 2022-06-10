fd1cf816a3edc838a196eeb78647953cd66c40ea
zevik7 <zevik2807@gmail.com>
2022-06-10 17:45:25 +0700

    feat: add shop page to routers

--- src/components/Route/index.js
+++ src/components/Route/index.js
@@ -2,17 +2,25 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import Home from '../../pages/Home'
+import Shop from '../../pages/Shop'
 import BicycleDetail from '../../pages/BicycleDetail'
 import BicycleManagement from '../../pages/Admin/BicycleManagement'
+import Login from '../../pages/Admin/Login'
+import Register from '../../pages/Admin/Register'
 import NoMatch from '../../pages/NoMatch'
 
 const RoutesApp = () => {
   return (
     <Routes>
+      {/* For Users */}
       <Route path="/" element={<Home />} />
-      <Route path="/bicycle-detail/:id" element={<BicycleDetail />} />
+      <Route path="/shop" element={<Shop />} />
+      <Route path="/shop/bicycle-detail/:id" element={<BicycleDetail />} />
 
-      <Route path="admin/bicycle-management" element={<BicycleManagement />} />
+      {/* For Admins */}
+      <Route path="/admin/bicycle-management" element={<BicycleManagement />} />
+      <Route path="/admin/login" element={<Login />} />
+      <Route path="/admin/register" element={<Register />} />
 
       <Route path="*" element={<NoMatch />} />
     </Routes>
