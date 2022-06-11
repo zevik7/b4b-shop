19d48badd860610e60dabef8fce8af3b76e44970
JC Naty <monnatyz@gmail.com>
2022-06-11 23:15:32 +0700

    feat: table to describe bicycle parts information

---
+++ src/components/Table/index.js
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import { Table } from 'antd'
+
+const TableCustom = (props) => {
+  const { data, cols, ...others } = props
+
+  return <Table dataSource={data} columns={cols} {...others} />
+}
+
+export default TableCustom
