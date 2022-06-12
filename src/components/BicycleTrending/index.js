48b849bc05b8fe80076126a590d45a21dcb2ed74
trucho <truc.ho@nfq.asia>
2022-06-12 20:13:18 +0700

    add slide show carousel

---
+++ src/components/BicycleTrending/index.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from 'react'
+import './index.less'
+
+const BicycleTrending = (props) => {
+  const { children, show } = props
+
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [length, setLength] = useState(children.length)
+
+  const [touchPosition, setTouchPosition] = useState(null)
+
+  // Set the length to match current children from props
+  useEffect(() => {
+    setLength(children.length)
+  }, [children])
+
+  const next = () => {
+    if (currentIndex < length - show) {
+      setCurrentIndex((prevState) => prevState + 1)
+    }
+  }
+
+  const prev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex((prevState) => prevState - 1)
+    }
+  }
+
+  const handleTouchStart = (e) => {
+    const touchDown = e.touches[0].clientX
+    setTouchPosition(touchDown)
+  }
+
+  const handleTouchMove = (e) => {
+    const touchDown = touchPosition
+
+    if (touchDown === null) {
+      return
+    }
+
+    const currentTouch = e.touches[0].clientX
+    const diff = touchDown - currentTouch
+
+    if (diff > 5) {
+      next()
+    }
+
+    if (diff < -5) {
+      prev()
+    }
+
+    setTouchPosition(null)
+  }
+
+  return (
+    <div className="trending">
+      <h2>Trending products</h2>
+      <div className="carousel-container">
+        <div className="carousel-wrapper">
+          {/* You can alwas change the content of the button to other things */}
+          {currentIndex > 0 && (
+            <button onClick={prev} className="left-arrow">
+              &lt;
+            </button>
+          )}
+          <div
+            className="carousel-content-wrapper"
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+          >
+            <div
+              className={`carousel-content show-${show}`}
+              style={{
+                transform: `translateX(-${currentIndex * (100 / show)}%)`,
+              }}
+            >
+              {children}
+            </div>
+          </div>
+          {/* You can alwas change the content of the button to other things */}
+          {currentIndex < length - show && (
+            <button onClick={next} className="right-arrow">
+              &gt;
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BicycleTrending
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,68 @@
 import { useState } from 'react'
+import BicycleCarousel from '../../components/BicycleCarousel'
+import BicycleTrending from '../../components/BicycleTrending'
+import BicycletCard from '../../components/BicycleCard'
 
 const Landing = () => {
   return (
     <>
-      <h1>This is landing page</h1>
+      <div className="carousel">
+        <BicycleCarousel />
+      </div>
+      <div className="product-trending">
+        {/* <BicycleTrending/> */}
+        <div
+          style={{
+            maxWidth: 1200,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            marginTop: 64,
+          }}
+        >
+          <BicycleTrending show={4}>
+            <div>
+              <div style={{ padding: 8 }}>
+                <BicycletCard />
+              </div>
+            </div>
+            <div>
+              <div style={{ padding: 8 }}>
+                <BicycletCard />
+              </div>
+            </div>
+            <div>
+              <div style={{ padding: 8 }}>
+                <BicycletCard />
+              </div>
+            </div>
+            <div>
+              <div style={{ padding: 8 }}>
+                <BicycletCard />
+              </div>
+            </div>
+            <div>
+              <div style={{ padding: 8 }}>
+                <BicycletCard />
+              </div>
+            </div>
+            <div>
+              <div style={{ padding: 8 }}>
+                <BicycletCard />
+              </div>
+            </div>
+            <div>
+              <div style={{ padding: 8 }}>
+                <BicycletCard />
+              </div>
+            </div>
+            <div>
+              <div style={{ padding: 8 }}>
+                <BicycletCard />
+              </div>
+            </div>
+          </BicycleTrending>
+        </div>
+      </div>
     </>
   )
 }
