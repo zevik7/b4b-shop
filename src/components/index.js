d957b34841da01f8c3daf9631fc5667e0315d0fd
huynhvangioiem <huynhvangioiem@gmail.com>
2022-06-12 20:04:24 +0700

    ffix: crename the BicycleTable to Bicycle for feature BicycleManagement

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { default as Sidebar } from './Admin/Sidebar'
 export { default as HeaderComponent } from './Admin/HeaderComponent'
-export { default as BicycleTable } from './Admin/Bicycle'
+export { default as Bicycle } from './Admin/Bicycle'
