94ef0b9ef791763e3c36417c9de4a61d97575c68
huynhvangioiem <huynhvangioiem@gmail.com>
2022-06-16 01:52:47 +0700

    fix: redesign layout for from add

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -3,3 +3,4 @@ import { createSelector } from 'reselect'
 export const bicyclesSelector = (state) => state.bicycles
 
 export const bicycleSelectedSelector = (state) => state.bicycles.selected
+export const bicycleDataSelector = (state) => state.bicycles.data
