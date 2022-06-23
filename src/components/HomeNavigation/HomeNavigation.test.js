6c38c57887e48be79d556b78359fef489a136c89
trucho <truc.ho@nfq.asia>
2022-06-24 02:41:43 +0700

    test/navigation: create navigation test

---
+++ src/components/HomeNavigation/HomeNavigation.test.js
@@ -0,0 +1,17 @@
+import HomeNavigation from "./index.js";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+    test("HomeNavigation should render", () => {
+    render(
+        <BrowserRouter>
+        <Routes>
+            <Route exact path="/" element={<HomeNavigation />} />
+        </Routes>
+        </BrowserRouter>
+    );
+    expect(screen.getByText("Bicycles")).toBeInTheDocument();
+    expect(screen.getByText("Home")).toBeInTheDocument();
+    });
